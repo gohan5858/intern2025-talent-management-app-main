31eb055fa87e1574c2f84cbe8ea4f2c88ed28ef0
grfsv <umequiqui@gmail.com>
2025-07-19 14:28:01 +0900

    [add] 戻るボタンのコンポーネントを追加し、社員詳細ページに追加

--- frontend/src/app/employee/page.tsx
+++ frontend/src/app/employee/page.tsx
@@ -1,14 +1,18 @@
+import { BackButton } from "@/components/BackButton";
 import { EmployeeDetailsContainer } from "@/components/EmployeeDetailsContainer";
 import { GlobalContainer } from "@/components/GlobalContainer";
-import { Suspense } from 'react';
+import { Suspense } from "react";
 
 export default function EmployeePage() {
   return (
-    <GlobalContainer pageName={"社員検索"}>
-      { /* Mark EmployeeDetailsContainer as CSR */ }
-      <Suspense>
-        <EmployeeDetailsContainer />
-      </Suspense>
-    </GlobalContainer>
+    <>
+      <GlobalContainer pageName={"社員検索"}>
+        {/* Mark EmployeeDetailsContainer as CSR */}
+        <Suspense>
+          <EmployeeDetailsContainer />
+        </Suspense>
+        <BackButton text={"検索画面に戻る"} />
+      </GlobalContainer>
+    </>
   );
 }
