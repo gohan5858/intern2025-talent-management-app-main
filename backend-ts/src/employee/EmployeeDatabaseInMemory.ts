042e346c4ea8008171942ba67976c2d9efec9956
aieuo <35859665+aieuo@users.noreply.github.com>
2025-07-19 11:44:27 +0900

    社員データに所属と役職を追加

--- backend-ts/src/employee/EmployeeDatabaseInMemory.ts
+++ backend-ts/src/employee/EmployeeDatabaseInMemory.ts
@@ -6,9 +6,9 @@ export class EmployeeDatabaseInMemory implements EmployeeDatabase {
 
     constructor() {
         this.employees = new Map<string, Employee>();
-        this.employees.set("1", { id: "1", name: "Jane Doe", age: 22 });
-        this.employees.set("2", { id: "2", name: "John Smith", age: 28 });
-        this.employees.set("3", { id: "3", name: "山田 太郎", age: 27 });
+        this.employees.set("1", { id: "1", name: "Jane Doe", age: 22, affiliation: "人事", position: "主任" });
+        this.employees.set("2", { id: "2", name: "John Smith", age: 28, affiliation: "人事", position: "部長" });
+        this.employees.set("3", { id: "3", name: "山田 太郎", age: 27, affiliation: "開発", position: "一般" });
     }
 
     async getEmployee(id: string): Promise<Employee | undefined> {
