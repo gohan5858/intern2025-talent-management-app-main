5c39e4ff14ca43a9d4ff2bb52f7fe4beef7d2ac0
gohan5858 <88976739+gohan5858@users.noreply.github.com>
2025-07-19 14:07:01 +0900

    マージ先のミスの修正 (#9)
    * Task<2>: <ウィンドウタイトルに、「社員検索」や「社員詳細」など、「自分が今どのページにいるのか」を表示する機能を追加> (#4)
    
    * グローバルヘッダに、「社員検索」や「社員詳細」など、「自分が今どのページにいるのか」を表示する機能を追加
    
    * 不要なパッケージを削除
    
    ---------
    
    Co-authored-by: Integral <tk.07@outlook.jp>
    
    * [fix] 初回ロード時にウィンドウタイトルが更新されない不具合を解消
    
    * [fix]
    
    * [fix] EmployeeDetails.tsxもtitleヘッダーの形に合わせました
    
    * fix: package.jsonと内容が同期していなかったのを修正 (#10)
    
    ---------
    
    Co-authored-by: Integral <110671141+Integral-07@users.noreply.github.com>
    Co-authored-by: Integral <tk.07@outlook.jp>

--- frontend/src/components/SearchEmployees.tsx
+++ frontend/src/components/SearchEmployees.tsx
@@ -6,24 +6,28 @@ import { EmployeeListContainer } from "./EmployeeListContainer";
 export function SearchEmployees() {
   const [searchKeyword, setSearchKeyword] = useState("");
   return (
-    <Paper
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        gap: 2,
-        flex: 1,
-        p: 2,
-      }}
-    >
-      <TextField
-        placeholder="検索キーワードを入力してください"
-        value={searchKeyword}
-        onChange={(e) => setSearchKeyword(e.target.value)}
-      />
-      <EmployeeListContainer
-        key="employeesContainer"
-        filterText={searchKeyword}
-      />
-    </Paper>
+    <>
+      <title>タレントマネジメントシステム - 社員検索</title>
+
+      <Paper
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: 2,
+          flex: 1,
+          p: 2,
+        }}
+      >
+        <TextField
+          placeholder="検索キーワードを入力してください"
+          value={searchKeyword}
+          onChange={(e) => setSearchKeyword(e.target.value)}
+        />
+        <EmployeeListContainer
+          key="employeesContainer"
+          filterText={searchKeyword}
+        />
+      </Paper>
+    </>
   );
 }
