49fa58d1f8dd99564cef059d13652b3faf934738
grfsv <umequiqui@gmail.com>
2025-07-20 16:16:09 +0900

    [fix] パスパラメータを非同期で取得する様に変更

--- frontend/src/app/employee/[id]/page.tsx
+++ frontend/src/app/employee/[id]/page.tsx
@@ -3,12 +3,18 @@ import { EmployeeDetailsContainer } from "@/components/EmployeeDetailsContainer"
 import { GlobalContainer } from "@/components/GlobalContainer";
 import { Suspense } from "react";
 
-export default function EmployeePage({ params }: { params: { id: string } }) {
+export default async function EmployeePage({
+  params,
+}: {
+  params: Promise<{ id: string }>; // 非同期で取得する
+}) {
+  const { id } = await params;
+  
   return (
     <GlobalContainer pageName={"社員詳細"}>
       {/* Mark EmployeeDetailsContainer as CSR */}
       <Suspense>
-        <EmployeeDetailsContainer id={params.id} />
+        <EmployeeDetailsContainer id={id} />
       </Suspense>
       <BackButton text={"検索画面に戻る"} />
     </GlobalContainer>
